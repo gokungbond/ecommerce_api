6b7d1a526a33e6a452ac407ec39d05654ebe677d
Patrick Joseph Encinares <patrickjoseph.greenapple@gmail.com>
2024-01-17 21:03:55 +0800

    FEAT : UNIT

---
+++ app/Http/Controllers/UnitController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Unit;
+use Illuminate\Http\Response;
+use App\Traits\HasApiResponses;
+use App\Http\Requests\StoreUnitRequest;
+use App\Http\Requests\UpdateUnitRequest;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Interfaces\MasterData\UnitInterface;
+
+class UnitController extends Controller
+{
+    use HasApiResponses;
+
+    protected UnitInterface $unitService;
+
+    public function __construct(UnitInterface $unitService)
+    {
+        $this->unitService = $unitService;
+    }
+
+    public function list()
+    {
+        $data = $this->unitService->list();
+
+        return $this->success(
+            $data,
+        );
+    }
+
+    public function show($id) 
+    {
+        $data = $this->unitService->show($id);
+
+        return $this->success(
+            $data,
+        );
+    }
+    public function store(StoreCategoryRequest $request) 
+    {
+        $data = $this->unitService->show(
+            $request->validated()
+        );
+
+        return $this->success(
+            $data,
+        );
+    }
+    public function edit($id) 
+    {
+        $data = $this->unitService->edit($id);
+
+        return $this->success(
+            $data,
+        );
+    }
+    public function update($id ,UpdateCategoryRequest $request) 
+    {   
+        $data = $this->unitService->update(
+            $id,
+            $request->validated()
+        );
+
+        return $this->success(
+            $data,
+        );
+    }
+    public function destory($id) 
+    {
+        $this->unitService->destory($id);
+
+        return $this->success(
+            [],
+            '',
+            Response::HTTP_NO_CONTENT
+        );
+    }
+}
